20c5aa3168e6731000abbbb72126b4107d03eb72
pingwinoss <damkuch@gmail.com>
2020-04-14 19:49:32 +0100

    fix

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("<int:movie_id>/", views.display_movie, name="display-movie"),
     path("<int:movie_id>/add_comment_form_handler/", views.add_comment_form_handler, name="add_comment_form_handler"),
-    path("<int:movie_id>/add_review_form_handler/", views.add_review_form_handler, name="add_review_form_handler"),
 ]
